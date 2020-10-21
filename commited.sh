d8246b9905a07a6a0262470cef2e3aab3d2c9ce2
Catalina Largo <katalinalargo24@gmail.com>
2020-10-21 14:30:26 -0500

    Añado unos assets faltantes

--- commited.sh
+++ commited.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 message=$1
-declare -a names=("Melanie Bochero" "Laura Cardenas" "Laura Cardenas" "Catalina Largo")
-declare -a emails=("melanieuwu28@gmail.com" "lauracard854@gmail.com" "katalinalargo24@gmail.com" "katalinalargo24@gmail.com")
-selected=$((0 + RANDOM % 4))
+declare -a names=("Melanie Bochero" "Catalina Largo")
+declare -a emails=("melanieuwu28@gmail.com" "katalinalargo24@gmail.com")
+selected=$((0 + RANDOM % 2))
 name=${names[$selected]}
 email=${emails[$selected]}
 echo "Running git commit command with the user ${name} ${email} and message: $message"
