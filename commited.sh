e6057f81f098789f90206af02d962d03c2a37383
Melanie Bochero <melanieuwu28@gmail.com>
2020-10-21 14:49:16 -0500

    Cambio ortografia y algunos mensajes

--- commited.sh
+++ commited.sh
@@ -2,7 +2,7 @@
 message=$1
 declare -a names=("Melanie Bochero")
 declare -a emails=("melanieuwu28@gmail.com")
-selected=$((0 + RANDOM % 2))
+selected=$((0 + RANDOM % 1))
 name=${names[$selected]}
 email=${emails[$selected]}
 echo "Running git commit command with the user ${name} ${email} and message: $message"
